8fefafde833a237b119f002aae7752a92d29f73c
michael <me@michaelfaith.dev>
2024-08-12 18:16:21 -0700

    Fixed typos, converted to docstrings, added a constructor overload to assume version 1.2 boards, and added sleep mode handling.

--- RFIDuino/RFIDuino.h
+++ RFIDuino/RFIDuino.h
@@ -26,41 +26,48 @@
 
 #include "Arduino.h"
 
-#ifndef RFIDuino_h //load header if we have not defined RFIDuino_h
-#define RFIDuino_h //define RFIDuino_h, to prevent code being loaded if this library is loaded a second time
+#ifndef RFIDuino_h                                                  // load header if we have not defined RFIDuino_h
+#define RFIDuino_h                                                  // define RFIDuino_h, to prevent code being loaded if this library is loaded a second time
 
-#define DELAYVAL    320   //384 //standard delay for manchster decode
-#define TIMEOUT     1000  //standard timeout for manchester decode
+#define DELAYVAL    320                                             // 384 //standard delay for manchster decode
+#define TIMEOUT     1000                                            // standard timeout for manchester decode
 
-class RFIDuino
-{
+class RFIDuino {
 	public:
 
-		RFIDuino(float verison);
-		//i/o
-		void errorSound(); 					//play an error sound to the buzzer
-		void successSound(); 				//play a sucess sound ot the buzzer
+		RFIDuino(float version = 1.2);
+		// i/o
+		void errorSound(); 					                        // play an error sound to the buzzer
+		void successSound(); 				                        // play a success sound ot the buzzer
 
-		//low level deocde functions
-		bool decodeTag(unsigned char *buf);	//low level tag decode
-		void transferToBuffer(byte *tagData, byte *tagDataBuffer);	//transfer data from one array to another
-		bool compareTagData(byte *tagData1, byte *tagData2);			//compate 2 arrays
+		// low level decode functions
+		bool decodeTag(unsigned char *buf);	                        // low level tag decode
+		void transferToBuffer(byte *tagData, byte *tagDataBuffer);	// transfer data from one array to another
+		bool compareTagData(byte *tagData1, byte *tagData2);		// compare 2 byte arrays
 
-		//higher level tag scanning / reporting
+		// higher level tag scanning / reporting
 		bool scanForTag(byte *tagData);
 
-		//user output pin values, variables as they will change depending on which hardware is used
+        // Sleep getter and setter
+        void setSleep(bool state);                                  // Changed shd pin state
+        bool getSleep();
+
+
+        // user output pin values, variables as they will change depending on which hardware is used
 		int buzzer;
 		int led1;
 		int led2;
 
 	private:
 
-		//pins to connect Arduino to the EM4095 chip , variables as they will change depending on which hardware is used
+		// pins to connect Arduino to the EM4095 chip, variables as they will change depending on which hardware is used
 		int demodOut;
 		int shd;
 		int mod;
 		int rdyClk;
+
+        // Cached sleep pin state
+        bool sleep;
 
 };
 
