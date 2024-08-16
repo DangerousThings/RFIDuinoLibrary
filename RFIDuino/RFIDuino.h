9808840905ac939d8eb18e13bb662a6c20fb82fb
dangerous-tac0s <operations@dangerousthings.com>
2024-08-16 09:55:09 -0700

    Update RFIDuino.h

--- RFIDuino/RFIDuino.h
+++ RFIDuino/RFIDuino.h
@@ -1,44 +1,35 @@
 /****************************************************************************************************
 * rfiduino.h - RFIDuino Library Header File
 *
-* RFIDuino Library v1.2 by TrossenRobotics / RobotGeek
+* RFIDuino Library v1.2 by TrossenRobotics / RobotGeek / Dangerous Things
 *
 *	This Library enables a Geekduino/Arduino to work with the RobotGeek RFIDuino Shield.
 *	Using the sheild and this library a user can detect a card, read/decode the card,
 *	compare the read to previous reads, and intearct with the inbord buzzer.
 *
 * Links
-*    RFIDuino Getting Started Guide
-*      http://learn.robotgeek.com/getting-started/41-rfiduino/142-rfiduino-getting-started-guide.html
-*    RFIDuino Library Documentation
-*      http://learn.robotgeek.com/41-rfiduino/144-rfiduino-library-documentation.html
 *    RFIDuino Shield Product Page
-*      http://www.robotgeek.com/rfiduino
-*
-*
-* Trossen Robotics/RobotGeek.com
+*      http://dngr.us/rfiduino
 *
 * Header file for the RFIDuino Library
-*  http://learn.robotgeek.com/getting-started/41-rfiduino/142-rfiduino-getting-started-guide.html
-*  http://learn.robotgeek.com/41-rfiduino/144-rfiduino-library-documentation.html
 *
 ****************************************************************************************************/
 
 #include "Arduino.h"
 
-#ifndef RFIDuino_h                                                  // load header if we have not defined RFIDuino_h
-#define RFIDuino_h                                                  // define RFIDuino_h, to prevent code being loaded if this library is loaded a second time
+#ifndef RFIDuino_h                                                  		// load header if we have not defined RFIDuino_h
+#define RFIDuino_h                                                  		// define RFIDuino_h, to prevent code being loaded if this library is loaded a second time
 
-#define DELAYVAL    320                                             // 384 //standard delay for manchster decode
-#define TIMEOUT     1000                                            // standard timeout for manchester decode
+#define DELAYVAL    320                                             		// 384 // standard delay for manchster decode
+#define TIMEOUT     1000                                            		// standard timeout for manchester decode
 
 class RFIDuino {
 	public:
 
 		RFIDuino(float version = 1.2);
 		// i/o
-		void errorSound(); 					                        // play an error sound to the buzzer
-		void successSound(); 				                        // play a success sound ot the buzzer
+		void errorSound(); 					        // play an error sound to the buzzer
+		void successSound(); 				                // play a success sound ot the buzzer
 
 		// low level decode functions
 		bool decodeTag(unsigned char *buf);	                        // low level tag decode
@@ -48,12 +39,12 @@ class RFIDuino {
 		// higher level tag scanning / reporting
 		bool scanForTag(byte *tagData);
 
-        // Sleep getter and setter
-        void setSleep(bool state);                                  // Changed shd pin state
-        bool getSleep();
+        	// Sleep getter and setter
+        	void setSleep(bool state);                                  	// Change shd pin state
+        	bool getSleep();
 
 
-        // user output pin values, variables as they will change depending on which hardware is used
+        	// user output pin values, variables as they will change depending on which hardware is used
 		int buzzer;
 		int led1;
 		int led2;
@@ -66,8 +57,8 @@ class RFIDuino {
 		int mod;
 		int rdyClk;
 
-        // Cached sleep pin state
-        bool sleep;
+        	// Cached sleep pin state
+        	bool sleep;
 
 };
 
